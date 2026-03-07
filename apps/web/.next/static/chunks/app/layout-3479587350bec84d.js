daa23f1be173b8a093ee27bafd5ab8ec3e811af4
Tom Cruise Missile <233243487+TomCruiseTorpedo@users.noreply.github.com>
2026-03-07 09:41:55 -0700

    chore: restore private paths to .gitignore (do not publish)

--- apps/web/.next/static/chunks/app/layout-3479587350bec84d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{970:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,6134,23))},6134:()=>{}},e=>{e.O(0,[341,814,743,358],()=>e(e.s=970)),_N_E=e.O()}]);
